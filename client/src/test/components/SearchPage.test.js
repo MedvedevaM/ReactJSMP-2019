9218488443ed78e1e15f06d4e9d808cfe0047f5d
Medvedeva <medvedeva.maria6991@gmail.com>
2019-04-03 21:29:53 +0300

    add click simulation for search page tests

--- client/src/test/components/SearchPage.test.js
+++ client/src/test/components/SearchPage.test.js
@@ -23,7 +23,8 @@ it("choosing film", () => {
             runtime: 100,
             tagline: "Tag line",
             overview: "The third film based on Marvel's Guardians of the Galaxy.",
-            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg"
+            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
+            genres: ['Drama', 'Adventure']
         }, {
             id: 2222,
             release_date: "2020-05-01",
@@ -32,7 +33,8 @@ it("choosing film", () => {
             runtime: 100,
             tagline: "Tag line",
             overview: "The third film based on Marvel's Guardians of the Galaxy.",
-            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg"
+            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
+            genres: ['Drama', 'Adventure']
         }]
     };
 
@@ -44,16 +46,17 @@ it("choosing film", () => {
     return searchPage.instance().fetchFilms()
         .then(() => {
             expect(searchPage.state().films).toEqual(mockData.data);
-            // searchPage.find(FilmsContainer).find(Film).find("[data-film-id='2222']").simulate('click');
-            // expect(searchPage.state().chosenFilm).toEqual({
-            //     id: "2222",
-            //     release_date: "2020-05-01",
-            //     title: "Guardians of the Galaxy Vol. 3",
-            //     vote_average: 8,
-            //     runtime: 100,
-            //     tagline: "Tag line",
-            //     overview: "The third film based on Marvel's Guardians of the Galaxy.",
-            //     poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg"
-            // });
+            searchPage.find(FilmsContainer).dive().find({ id: 2222 }).dive().find("h3").simulate('click');
+            searchPage.update();
+            expect(searchPage.state().chosenFilm).toEqual({
+                id: "2222",
+                release_date: "2020-05-01",
+                title: "Guardians of the Galaxy Vol. 3",
+                vote_average: 8,
+                runtime: 100,
+                tagline: "Tag line",
+                overview: "The third film based on Marvel's Guardians of the Galaxy.",
+                poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg"
+            });
         });
 });
