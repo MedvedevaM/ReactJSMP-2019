e4bda39029c28b54824f1762cd798a079075f3fa
Medvedeva <medvedeva.maria6991@gmail.com>
2019-04-05 00:54:59 +0300

    add some corrections

--- client/src/test/components/RadioButtons.test.js
+++ client/src/test/components/RadioButtons.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { shallow, configure } from "enzyme";
-import { RadioButtons } from "../../main/components/RadioButtons.jsx";
 import Adapter from "enzyme-adapter-react-16";
+import toJson from 'enzyme-to-json';
+import { RadioButtons } from "../../main/components/RadioButtons.jsx";
 
 configure({ adapter: new Adapter() });
 
@@ -9,7 +10,7 @@ const parameters = ["first_parameter", "second_parameter", "third_parameter"];
 
 it("check radio buttons rendering", () => {
     const radioButtons = shallow( <RadioButtons parameters={parameters} searchBy="second_parameter" /> );
-    expect(radioButtons).toMatchSnapshot();
+    expect(toJson(radioButtons)).toMatchSnapshot();
     expect(radioButtons.find('input').length).toBe(3);
 });
 
--- client/src/test/components/SearchPage.test.js
+++ client/src/test/components/SearchPage.test.js
@@ -1,62 +1,90 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
-import { SearchPage } from "../../main/components/SearchPage.jsx";
-import { Film } from "../../main/components/Film";
+import { shallow, configure, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { fetch } from 'whatwg-fetch';
-import { FilmsContainer } from "../../main/components/FilmsContainer.jsx";
+import toJson from 'enzyme-to-json';
+import { SearchPage } from "../../main/components/SearchPage.jsx";
 
 configure({ adapter: new Adapter() });
+describe("search page rendering and functionality", () => {
+    it("check radio buttons rendering", () => {
+        const searchPage = shallow( <SearchPage /> );
+        expect(toJson(searchPage)).toMatchSnapshot();
+    });
 
-it("check radio buttons rendering", () => {
-    const searchPage = shallow( <SearchPage /> );
-    expect(searchPage).toMatchSnapshot();
-});
-
-it("choosing film", () => {
-    const mockData = {
-        data: [{
-            id: 1111,
-            release_date: "2020-05-01",
-            title: "Guardians of the Galaxy Vol. 3",
-            vote_average: 8,
-            runtime: 100,
-            tagline: "Tag line",
-            overview: "The third film based on Marvel's Guardians of the Galaxy.",
-            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
-            genres: ['Drama', 'Adventure']
-        }, {
-            id: 2222,
-            release_date: "2020-05-01",
-            title: "Guardians of the Galaxy Vol. 3",
-            vote_average: 8,
-            runtime: 100,
-            tagline: "Tag line",
-            overview: "The third film based on Marvel's Guardians of the Galaxy.",
-            poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
-            genres: ['Drama', 'Adventure']
-        }]
-    };
-
-    jest.spyOn(window, 'fetch').mockImplementation(() => Promise.resolve({
-        json: () => Promise.resolve(mockData),
-    }));
-    const searchPage = shallow(<SearchPage />, { disableLifecycleMethods: true });
-
-    return searchPage.instance().fetchFilms()
-        .then(() => {
-            expect(searchPage.state().films).toEqual(mockData.data);
-            searchPage.find(FilmsContainer).dive().find({ id: 2222 }).dive().find("h3").simulate('click');
-            searchPage.update();
-            expect(searchPage.state().chosenFilm).toEqual({
-                id: "2222",
+    it("choosing film", () => {
+        const mockData = {
+            data: [{
+                id: 1111,
                 release_date: "2020-05-01",
                 title: "Guardians of the Galaxy Vol. 3",
                 vote_average: 8,
                 runtime: 100,
                 tagline: "Tag line",
                 overview: "The third film based on Marvel's Guardians of the Galaxy.",
-                poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg"
+                poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
+                genres: ['Drama', 'Adventure']
+            }, {
+                id: 2222,
+                release_date: "2020-05-01",
+                title: "Guardians of the Galaxy Vol. 3",
+                vote_average: 8,
+                runtime: 100,
+                tagline: "Tag line",
+                overview: "The third film based on Marvel's Guardians of the Galaxy.",
+                poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
+                genres: ['Drama', 'Adventure']
+            }]
+        };
+
+
+        jest.spyOn(window, 'fetch').mockImplementation(() => Promise.resolve({
+            json: () => Promise.resolve(mockData),
+        }));
+
+        const searchPage = mount(<SearchPage />);
+        searchPage.update();
+
+        return searchPage.instance().fetchFilms()
+            .then(() => {
+                expect(searchPage.state().films).toEqual(mockData.data);
+                searchPage.instance().onSortParameterClick({
+                    target: {
+                        dataset: {
+                            sortParameter: "rating"
+                        }
+                    }
+                });
+                expect(searchPage.state().sortBy).toEqual("rating");
+                searchPage.instance().onSearchParameterClick({
+                    target: {
+                        dataset: {
+                            parameter: "Genre"
+                        }
+                    }
+                });
+                expect(searchPage.state().searchBy).toEqual("Genre");
+                searchPage.instance().onSearchModeClick();
+                expect(searchPage.state().chosenFilm).toEqual(null);
+                expect(searchPage.state().sortBy).toEqual("rating");
+                searchPage.instance().chooseFilm({
+                    target: {
+                        dataset: {
+                            filmId: 2222
+                        }
+                    }
+                });
+                expect(searchPage.state().chosenFilm).toEqual({
+                    id: 2222,
+                    release_date: "2020-05-01",
+                    title: "Guardians of the Galaxy Vol. 3",
+                    vote_average: 8,
+                    runtime: 100,
+                    tagline: "Tag line",
+                    overview: "The third film based on Marvel's Guardians of the Galaxy.",
+                    poster_path: "https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg",
+                    genres: ['Drama', 'Adventure']
+                });
             });
-        });
-});
+    });
+})
