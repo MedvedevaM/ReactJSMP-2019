14e28eefbb1a00df6dea54f5835073614a93207c
Medvedeva <medvedeva.maria6991@gmail.com>
2019-04-07 15:34:41 +0300

    add corrections of e2e

--- cypress/integration/sample.cypress.js
+++ cypress/integration/sample.cypress.js
@@ -4,7 +4,7 @@ describe('The Home Page', () => {
     cy.visit('http://localhost:8080');
     cy.get('input[data-parameter=Genre]').click();
     cy.get('li[data-sort-parameter=rating]').click();
-    cy.get('img[data-film-id=412302]').click();
+    cy.get('.film').first().find('img').click();
     cy.get('button.search-button-default-mode').click();
     cy.get('input[type=text]').type('search');
   });
